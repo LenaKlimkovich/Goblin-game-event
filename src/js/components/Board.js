e6fdcda38ec38bde16ac59c168f003318e82e569
LKl <plat.elena2010@ya.ru>
2026-05-06 11:19:16 +0300

    Revised as per comments

--- src/js/components/Board.js
+++ src/js/components/Board.js
@@ -1,16 +1,17 @@
 import goblin from '../../pic/goblin.png';
 
 const game = document.getElementById('game');
+const defaultBoardSize = 16;
 
 export default class Board {
-  constructor(boardSize = 16) {
+  constructor(boardSize = defaultBoardSize) {
     this.boardSize = boardSize;
     this.cells = [];
 
     for (let i = 0; i < boardSize; i++) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
-      game.appendChild(cell);
+      game.append(cell);
       this.cells.push(cell);
     }
 
--- src/js/components/Game.js
+++ src/js/components/Game.js
@@ -1,56 +1,78 @@
 import Board from "./Board";
 
 const game = document.getElementById('game');
+const your_scores = document.querySelector('.your_scores');
+const your_failures = document.querySelector('.your_failures');
 
 export default class Game {
-  constructor() {
-    this.board = new Board();
-    this.score = 0;
-    this.missed = 0;
-    this.currentIndex = -1;
-    this.game = game;
+    constructor() {
+        this.board = new Board();
+        this.score = 0;
+        this.missed = 0;
+        this.fatalScore = 5;
+        this.currentIndex = -1;
+        this.game = game;
 
-    this.start();
-    this.initClickHandler();
-  }
-
-  initClickHandler() {
-    this.game.addEventListener('click', (e) => {
-      const cell = e.target.closest('.cell');
-      if (!cell) return;
-
-      const clickedIndex = this.board.cells.indexOf(cell);
-
-      if (clickedIndex === this.currentIndex) {
-        this.score++;
-        cell.innerHTML = "";
-        this.currentIndex = -1; 
-      }
-    });
-  }
-
-  start() {
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  tick() {
-
-    if (this.currentIndex !== -1) {
-      this.missed++;
-
-      if (this.missed >= 5) {
-        clearInterval(this.interval);
-        alert(`Вы проиграли!`);
-        return;
-      }
+        this.start();
+        this.initClickHandler();
     }
 
-    let newIndex = this.currentIndex;
-    while (newIndex === this.currentIndex) {
-      newIndex = Math.floor(Math.random() * this.board.boardSize);
+    showModal(message) {
+        const modal = document.getElementById('gameModal');
+        const modalText = document.getElementById('modalText');
+        modalText.innerText = message;
+        modal.style.display = 'flex';
+
+        document.getElementById('modalBtn').onclick = () => {
+            modal.style.display = 'none';
+            this.restartGame();
+        };
+    }
+    initClickHandler() {
+        this.game.addEventListener('click', (e) => {
+            const cell = e.target.closest('.cell');
+            if (!cell) return;
+
+            const clickedIndex = this.board.cells.indexOf(cell);
+
+            if (clickedIndex === this.currentIndex) {
+                ++this.score;
+                your_scores.innerText = `Вы попали ${this.score} раз(а)`;
+                cell.innerHTML = "";
+                this.currentIndex = -1;
+            }
+        });
     }
 
-    this.currentIndex = newIndex;
-    this.board.positionGoblin(newIndex);
-  }
+    start() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    tick() {
+        if (this.missed >= this.fatalScore) {
+            clearInterval(this.interval);
+            this.showModal('Вы проиграли!');
+            return;
+        }
+        if (this.currentIndex !== -1) {
+            this.missed++;
+            your_failures.innerText = `Вы промахнулись ${this.missed} раз(а)`;
+        }
+
+        let newIndex = this.currentIndex;
+        while (newIndex === this.currentIndex) {
+            newIndex = Math.floor(Math.random() * this.board.boardSize);
+        }
+
+        this.currentIndex = newIndex;
+        this.board.positionGoblin(newIndex);
+    }
+    restartGame() {
+        this.score = 0;
+        this.missed = 0;
+        this.currentIndex = -1;
+        your_scores.innerText = 'Вы попали 0 раз(а)';
+        your_failures.innerText = 'Вы промахнулись 0 раз(а)';
+        this.start();
+    }
 }
